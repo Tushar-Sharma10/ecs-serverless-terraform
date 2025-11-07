a463d6a232cdae9644663f8b483e9ba5967c9af2
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-11-07 16:39:08 -0500

    Add IAM roles and policies for ECS task execution and CloudWatch access

---
+++ iam.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsondecode({
+    version = "2012-10-17"
+    statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_cloudwatch_full_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskAppRole"
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
